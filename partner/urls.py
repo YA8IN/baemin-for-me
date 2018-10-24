32bb20cb0f2cf2a00fe86938593431c0ad7375c4
unknown <you@example.com>
2018-10-24 15:14:58 +0800

    2018-10-24

--- partner/urls.py
+++ partner/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from .views import index, signup, login, logout, edit_info
+from .views import index, signup, login, logout, edit_info, menu, menu_add
 
 urlpatterns = [
      url(r'^$', index, name="index"),
@@ -7,4 +7,6 @@ urlpatterns = [
      url(r'^login/$', login, name="login"),
      url(r'^logout/$', logout, name="logout"),
      url(r'^edit/$', edit_info, name="edit"),
+     url(r'^menu/$', menu, name="menu"),
+     url(r'^menu/add/$', menu_add, name="menu_add"),
 ]
